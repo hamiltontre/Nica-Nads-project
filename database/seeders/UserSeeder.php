7c6d67e644630f9e3f4ae751c0f2f884e1d3e885
hamiltontre <hgtreminio@uamv.edu.ni>
2025-07-01 09:30:55 -0700

    Se agregaron tres cuentas de prueba para el proyecto

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,19 +4,31 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run()
-{
-    \App\Models\User::create([
-        'name' => 'Admin Principal',
-        'email' => 'hgtreminio@nads.com',
-        'password' => bcrypt('ClaveSegura123'),
-        'email_verified_at' => now()
-    ]);
-}
+    {
+        User::create([
+            'name' => 'Hamilton Treminio',
+            'email' => 'HamiltonTI@nads.com',
+            'password' => bcrypt('123'),
+            'email_verified_at' => now()
+        ]);
+
+        User::create([
+            'name' => 'Paola Perez',
+            'email' => 'PaolaPZ@nads.com',
+            'password' => bcrypt('456'),
+            'email_verified_at' => now()
+        ]);
+
+        User::create([
+            'name' => 'Tymothy Hayes',
+            'email' => 'TimmyHY@nads.com',
+            'password' => bcrypt('789'),
+            'email_verified_at' => now()
+        ]);
+    }
 }
